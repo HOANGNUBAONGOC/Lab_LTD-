f98cdd1084de873bc8495e6abb03cd806d6b304d
HOANGNUBAONGOC <ngochnb.23ns@vku.udn.vn>
2025-02-17 16:55:37 +0700

    final

--- calculator/app/build.gradle.kts
+++ calculator/app/build.gradle.kts
@@ -6,7 +6,8 @@ plugins {
 
 android {
     namespace = "com.example.caculator"
-    compileSdk = 34
+    compileSdk = 35
+
 
     defaultConfig {
         applicationId = "com.example.caculator"
@@ -49,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
